3f989f46671ed3124c20cafddfed06a5e9fd97f4
Harsh Tiwari <harshtiwari343@email.com>
2025-11-10 17:28:28 +0530

    Added Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Welcome } from './welcome/welcome';
+import { ProductList } from './product-list/product-list';
+import { ProductDetail } from './product-detail/product-detail';
+import { Star } from './star/star';
+import { Title } from './title/title';
+import { About } from './about/about';
+import { Hello } from './hello/hello';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Welcome, ProductList, ProductDetail, Star, Title, About, Hello],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/welcome/welcome.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome', // name of the component
+  imports: [],
+  templateUrl: './welcome.html',
+  styleUrl: './welcome.scss',
+})
+export class Welcome {
+
+}
