22c629eb017f90f047f2209073a9e6e44ac9ff5b
Harsh Tiwari <harshtiwari343@email.com>
2025-11-14 17:19:16 +0530

    Subject and Behavior subject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Welcome } from './welcome/welcome';
 import { Title } from './title/title';
 import { CategoryList } from './categories/category-list/category-list';
 import { Count } from './count/count';
+import { Demo1 } from './demo1/demo1';
+import { Demo2 } from './demo2/demo2';
 
 export const routes: Routes = [
     { path: 'welcome', component: Count },
@@ -15,4 +17,6 @@ export const routes: Routes = [
     { path: 'users', loadComponent: () => import('./user-list/user-list').then(r => r.UserList) },
     { path: 'title', component: Title },
     { path: 'categories', component: CategoryList },
+    { path: 'demo1', component: Demo1 },
+    { path: 'demo2', component: Demo2 },
 ];
